eca35ea6ff31b0821d2caf02f110e7507fce4e3d
DonutApocalypse <smsovereign3@gmail.com>
2020-03-05 21:36:35 -0700

    <----- THIS ONE MOM

---
+++ GETSNAKE.bat
@@ -0,0 +1,12 @@
+ECHO "Creating new directory..."
+ECHO "Creating new directory..."
+ECHO "Creating new directory..."
+mkdir SnakeGame
+ECHO "|||||||||| 100%"
+ECHO "Moving snake to new directory."
+ECHO "Moving snake to new directory.."
+ECHO "Moving snake to new directory..."
+MOVE C:\Users\Sam\Downloads\Snake.jar C:\Users\Sam\Desktop\SnakeGame
+ECHO "|||||||||| 100%"
+ECHO "Find the folder on your desktop called SnakeGame. It is there"
+pause
